9b297e7965b38be715b20a5c9df1e3a18f5845c5
Utkarsh Singh <utkarsh.s@automationanywhere.com>
2023-07-14 16:45:17 +0530

    first commit

---
+++ src/main/java/com/automationanywhere/soap3/WsdlController.java
@@ -0,0 +1,18 @@
+package com.automationanywhere.soap3;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WsdlController {
+
+    @GetMapping(value = "/soap.wsdl", produces = MediaType.APPLICATION_XML_VALUE)
+    @ResponseBody
+    public Resource getWsdl() {
+        return new ClassPathResource("calculator.wsdl");
+    }
+}
---
+++ src/test/java/com/automationanywhere/soap3/Soap3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automationanywhere.soap3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soap3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
